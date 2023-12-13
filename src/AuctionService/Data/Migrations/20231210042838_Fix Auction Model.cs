6560c3b69807433b33d0cc013b74842e169ad783
Ted Fisher <tfisher0527@gmail.com>
2023-12-12 18:24:49 -0600

    add auctions api

---
+++ src/AuctionService/Data/Migrations/20231210042838_Fix Auction Model.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuctionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixAuctionModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "status",
+                table: "Auctions",
+                newName: "Status");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AuctionEnd",
+                table: "Auctions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AuctionEnd",
+                table: "Auctions");
+
+            migrationBuilder.RenameColumn(
+                name: "Status",
+                table: "Auctions",
+                newName: "status");
+        }
+    }
+}
